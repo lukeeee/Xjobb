f4e39cc5de5074406b8874c6b97233b3b35424a7
Emil Svensson <svenne87@gmail.com>
2013-12-09 15:04:08 +0100

    User class

---
+++ CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/User.java
@@ -0,0 +1,53 @@
+package se.xjobb.scardfeud;
+
+/**
+ * Created by Svempa on 2013-12-09.
+ */
+public class User {
+
+    private String username;
+    private String password;
+    private String identifier;
+    private int id;
+    private String countryCode;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+}
