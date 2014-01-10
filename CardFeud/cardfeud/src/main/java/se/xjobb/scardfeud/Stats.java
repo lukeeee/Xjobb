057a02f4f74dfb593b078103cc35766b33c2113c
Lukas Andersson <lukas_andersson@live.se>
2014-01-10 14:43:49 +0100

    new deck of card, plus start card in gamplay

--- CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/Stats.java
+++ CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/Stats.java
@@ -2,18 +2,26 @@ package se.xjobb.scardfeud;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
-public class Stats extends Activity {
+public class Stats extends Activity implements View.OnClickListener {
 
-
+private Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stats_layout);
+        start = (Button)findViewById(R.id.start_game);
+        start.setOnClickListener(this);
 
 
     }
-
-
+    public void onClick(View view) {
+        if (view == start){
+            start.setVisibility(Button.INVISIBLE);
+        }
+    }
 }
