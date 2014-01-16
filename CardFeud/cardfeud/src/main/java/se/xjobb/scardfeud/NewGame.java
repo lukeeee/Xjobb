af3c498017cd5e0b8d706aa8a7eb4233992870ee
Emil Svensson <svenne87@gmail.com>
2014-01-16 15:16:30 +0100

    Search part 1

--- CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/NewGame.java
+++ CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/NewGame.java
@@ -2,11 +2,13 @@ package se.xjobb.scardfeud;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class NewGame extends Activity {
+public class NewGame extends Activity implements View.OnClickListener{
     TextView user;
     TextView friends;
     Button search_player;
@@ -20,21 +22,36 @@ public class NewGame extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newgame);
-    user = (TextView)findViewById(R.id.user2);
-    friends = (TextView)findViewById(R.id.friends);
-    search_player = (Button)findViewById(R.id.search_player);
-    random_player = (Button)findViewById(R.id.random_player);
-    one = (Button)findViewById(R.id.one);
-    two = (Button)findViewById(R.id.two);
-    three = (Button)findViewById(R.id.three);
-    four = (Button)findViewById(R.id.four);
-    search_player.getBackground().setAlpha(150);
-    user.getBackground().setAlpha(150);
-    friends.getBackground().setAlpha(150);
-    random_player.getBackground().setAlpha(150);
-    one.getBackground().setAlpha(150);
-    two.getBackground().setAlpha(150);
-    three.getBackground().setAlpha(150);
-    four.getBackground().setAlpha(150);
+        user = (TextView)findViewById(R.id.user2);
+        friends = (TextView)findViewById(R.id.friends);
+        search_player = (Button)findViewById(R.id.search_player);
+        random_player = (Button)findViewById(R.id.random_player);
+        one = (Button)findViewById(R.id.one);
+        two = (Button)findViewById(R.id.two);
+        three = (Button)findViewById(R.id.three);
+        four = (Button)findViewById(R.id.four);
+        search_player.getBackground().setAlpha(150);
+        user.getBackground().setAlpha(150);
+        friends.getBackground().setAlpha(150);
+        random_player.getBackground().setAlpha(150);
+        one.getBackground().setAlpha(150);
+        two.getBackground().setAlpha(150);
+        three.getBackground().setAlpha(150);
+        four.getBackground().setAlpha(150);
+
+        search_player.setOnClickListener(this);
+    }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == search_player){
+            Intent i = new Intent(getBaseContext(), Search.class);
+            startActivity(i);
+            this.finish();
+        }
     }
 }
---
+++ CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/Search.java
@@ -0,0 +1,20 @@
+package se.xjobb.scardfeud;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Svempa on 2014-01-16.
+ */
+public class Search extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_layout);
+
+
+    }
+
+
+}
