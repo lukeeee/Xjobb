a93e9c5dd6a6b3a3543f2aa38b3716bc7ba7b1e2
Lukas Andersson <lukas_andersson@live.se>
2014-01-17 12:10:22 +0100

    stats navigationtabs

---
+++ CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/MyStats.java
@@ -0,0 +1,25 @@
+package se.xjobb.scardfeud;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Lukas on 2014-01-17.
+ */
+public class MyStats extends Fragment {
+    TextView rules;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.rules_layout, container, false);
+        rules = (TextView)rootView.findViewById(R.id.rules);
+        rules.getBackground().setAlpha(150);
+
+        return rootView;
+    }
+}
