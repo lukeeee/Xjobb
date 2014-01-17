28b63f6c53ae5684fc8be9c93d002bbe5491b60c
Lukas Andersson <lukas_andersson@live.se>
2014-01-17 15:20:40 +0100

    private in helperclass

--- CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/HelperClass.java
+++ CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/HelperClass.java
@@ -18,7 +18,7 @@ public class HelperClass {
     private final String PREFSUSERID = "cardFeudUserId";
     private final String PREFSIDENTIFIER = "cardFeudIdentifier";
     private final String PREFSDEVICEREGID = "cardFeudDeviceId";
-    public static final String PREFSUSERNAME = "cardFeudUsername";
+    private String PREFSUSERNAME = "cardFeudUsername";
     private final String PREFSCOUNTRYCODE = "cardFeudCountryCode";
 
     // Constructor to set Context
