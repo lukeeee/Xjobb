ac924487d4bf3568aed99081df79986de558d215
Lukas Andersson <lukas_andersson@live.se>
2014-01-30 13:36:41 +0100

    ads in rules

--- CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/Rules.java
+++ CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/Rules.java
@@ -1,20 +1,23 @@
 package se.xjobb.scardfeud;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 /**
  * Created by Lukas on 2014-01-08.
  */
 public class Rules extends Fragment {
 TextView rules;
+LinearLayout rulesLnrMain;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,6 +26,20 @@ TextView rules;
         rules = (TextView)rootView.findViewById(R.id.rules);
         rules.getBackground().setAlpha(150);
 
-        return rootView;
+        rulesLnrMain = (LinearLayout)rootView.findViewById(R.id.rulesLnrMain);
+
+
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                AdView adView = new AdView(getActivity());
+                adView.setAdUnitId("0445b7141d9d4e1b");
+                adView.setAdSize(AdSize.BANNER);
+                AdRequest.Builder builder = new AdRequest.Builder();
+                builder.addTestDevice(AdRequest.DEVICE_ID_EMULATOR);
+                adView.loadAd(builder.build());
+                rulesLnrMain.addView(adView);
+            }
+        }); return rootView;
     }
 }
