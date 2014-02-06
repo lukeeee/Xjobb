b4c042df5221435c87cae445e3a824abac59cd4b
Lukas Andersson <lukas_andersson@live.se>
2014-02-06 11:50:53 +0100

    pusheli push

--- CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/FinishedGameAdapter.java
+++ CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/FinishedGameAdapter.java
@@ -1,6 +1,7 @@
 package se.xjobb.scardfeud;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -21,8 +23,8 @@ public class FinishedGameAdapter extends BaseAdapter{
     ArrayList<Response> finishedGames;
     Context context;
     View.OnClickListener finListener;
-    Drawable win = context.getResources().getDrawable(R.drawable.win);
-    Drawable loose = context.getResources().getDrawable(R.drawable.loose);
+    Drawable win = context.getResources().getDrawable(R.drawable.game_win);
+    Drawable loose = context.getResources().getDrawable(R.drawable.game_lost);
 
     public FinishedGameAdapter(Context context, View.OnClickListener finListener){
         this.context = context;
@@ -51,6 +53,8 @@ public class FinishedGameAdapter extends BaseAdapter{
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 "fonts/hobostd.otf");
         Button finish = (Button)view.findViewById(R.id.finGameBtn);
+        ImageView gameResultIMG = (ImageView)view.findViewById(R.id.gameResult_img);
+        ImageView finFlag = (ImageView)view.findViewById(R.id.finFlag);
         if (view == null) {
             LayoutInflater infalInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = infalInflater.inflate(R.layout.waitgame_list, null);
@@ -63,6 +67,18 @@ public class FinishedGameAdapter extends BaseAdapter{
 
 
         Response response = GameListResult.getFinishedGames().get(i);
+        try {
+            String country = response.opponentName.toLowerCase();
+            int id = context.getResources().getIdentifier(country, "drawable", context.getPackageName());
+            Drawable drawable = context.getResources().getDrawable(id);
+            finFlag.setImageDrawable(drawable);
+        } catch (Resources.NotFoundException ex) {
+            // if the flag can't be found
+            int id = context.getResources().getIdentifier("globe", "drawable", context.getPackageName());
+            Drawable drawable = context.getResources().getDrawable(id);
+
+            finFlag.setImageDrawable(drawable);
+        }
         finish.setText("You " + "won/lost" +" against "+ response.opponentName);
         finish.setTypeface(tf);
 
