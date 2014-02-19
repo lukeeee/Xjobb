973656d173e8b71b7da26639026943a32b115ca0
Lukas Andersson <lukas_andersson@live.se>
2014-02-19 14:00:31 +0100

    push

--- CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/Start.java
+++ CardFeud/cardfeud/src/main/java/se/xjobb/scardfeud/Start.java
@@ -7,7 +7,6 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
